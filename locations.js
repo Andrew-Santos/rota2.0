26d637670db13cf987aa805da5eaa370976c28d5
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2025-02-20 14:13:32 -0300

    Update locations.js

--- locations.js
+++ locations.js
@@ -3,12 +3,14 @@
 
 export const locations = [
     {coords:[-12.876344,-40.405964], name:"MARIA APARECIDA ARRUDA DOS SANTOS", whatsapp:"557591109944", color:'grey', local:"REGIÃO DO MALHADOR"},
-    {coords:[-12.993759,-40.578120], name:"CILENE PEREIRA SENA DOS ANJOS", whatsapp:"+55 77 8155-0203", color:'grey', local:"BEIRA RIO"},
-    {coords:[-12.667898,-40.616274], name:"NORMACI DE JESUS CARDOSO", whatsapp:"+55 75 8157-3351", color:'grey', local:"CAMELITO"},
-    {coords:[-12.657858,-40.607921], name:"ALCINETE ALMEIDA DE SILVA", whatsapp:"+55 75 8198-8527", color:'grey', local:"PRAÇA MANOEL GARRINCHA"},
-    {coords:[-12.599496,-40.700189], name:"LIVANDETE BARROS DA SILVA", whatsapp:"+55 75 8130-1465", color:'grey', local:"ASSENTAMENTO POLINESIA"},
-    {coords:[-12.658835,-40.607746], name:"ANA LUISA SANTOS NASCIMENTO", whatsapp:"+55 75 8172-8393", color:'grey', local:"PRAÇA RUY BARBOSA"},
-    {coords:[-12.962843,-40.719296], name:"FERNANDA LIMA DE JESUS", whatsapp:"+55 75 8898-9903", color:'grey', local:"TERRA BOA"},
-    {coords:[-12.662184, -40.603792], name:"SANDRA SILVA DE SOUZA SAMPAIO", whatsapp:"+55 75 8831-9205", color:'grey', local:"NOVA BRASILIA"},
+    {coords:[-12.993759,-40.578120], name:"CILENE PEREIRA SENA DOS ANJOS", whatsapp:"557781550203", color:'grey', local:"BEIRA RIO"},
+    {coords:[-12.667898,-40.616274], name:"NORMACI DE JESUS CARDOSO", whatsapp:"557581573351", color:'grey', local:"CAMELITO"},
+    {coords:[-12.657858,-40.607921], name:"ALCINETE ALMEIDA DE SILVA", whatsapp:"557581988527", color:'grey', local:"PRAÇA MANOEL GARRINCHA"},
+    {coords:[-12.599496,-40.700189], name:"LIVANDETE BARROS DA SILVA", whatsapp:"557581301465", color:'grey', local:"ASSENTAMENTO POLINESIA"},
+    {coords:[-12.658835,-40.607746], name:"ANA LUISA SANTOS NASCIMENTO", whatsapp:"557581728393", color:'grey', local:"PRAÇA RUY BARBOSA"},
+    {coords:[-12.962843,-40.719296], name:"FERNANDA LIMA DE JESUS", whatsapp:"557588989903", color:'grey', local:"TERRA BOA"},
+    {coords:[-12.662184, -40.603792], name:"SANDRA SILVA DE SOUZA SAMPAIO", whatsapp:"557588319205", color:'grey', local:"NOVA BRASILIA"},
+    {coords:[-12.452195, -40.631293], name:"JULIANA QUINTINO COSTA", whatsapp:"557581508805", color:'grey', local:"AMPARO ZUCA"},
+    {coords:[-12.851935, -40.521389], name:"MARIA IONETE MARQUES DOS SANTOS DE JE", whatsapp:"75983154777U", color:'grey', local:"RURAL AMPARO"},
 
 ];

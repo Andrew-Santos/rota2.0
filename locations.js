e51f9e6c8476a448aef4b9da7f9fc243f2d528cd
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2025-02-20 13:23:17 -0300

    Update locations.js

--- locations.js
+++ locations.js
@@ -2,13 +2,6 @@
 
 
 export const locations = [
-    {coords:[-9.989255, -39.512338], name:"Diego Aleixo Da Silva", whatsapp:"5574998115205", color:'red', local:"Bairro:Povoado zona rural - Endereço:FAZENDA JABOTICABA, sn - Ref.:Casa da Lurdes"},
-    {coords:[-9.988568, -39.512877], name:"Manoel Gomes De Andrade", whatsapp:"5574999390102", color:'red', local:"Bairro:Zona Rural - Endereço:Povoado Queimadas do Jerônimo , 216 - Ref.:Depois da casa do Duca / Próximo o povoado poço da pedra  / 74 99953-2032 ( Arthur )"},
-    {coords:[-9.897691, -39.292495], name:"Wandemberg Santana De Macedo", whatsapp:"5511943326778", color:'red', local:"Bairro:Zona Rural - Endereço:Fazenda Caititus , sn - Ref.:Próximo ao povoado de carataca / Próximo fazenda poço dos cavalos / 2tel.: 74999437974 - Rita (mãe) "},
-    {coords:[-9.897492, -39.292273], name:"Romerio Macedo De Souza", whatsapp:"5574999185577", color:'red', local:"Bairro:interior - Endereço:Fazenda matrona, sn - Ref.:Logo após o Rio vaza Barris// 75991439774 Juscileide Mãe//vizinho a casa de dona Judite de adrião"},
-    {coords:[-9.884725, -39.591104], name:"Lourival De Souza Loiola", whatsapp:"5574999965564", color:'red', local:"Bairro:Fazenda travessa - Endereço:Fazenda travessa  Casa de lorival, sn - Ref.:Depois do rio // Fica próximo a escola são pedro"},
-    {coords:[-9.843664, -39.474679], name:"Daniele Elpidio Dias", whatsapp:"5574999597389", color:'red', local:"Bairro:Bairro da lagoa - Endereço:Rua Manoel quintino de silveira, 142 - Ref.:Próxima a igrejinha da rua da Lagoa / Próximo ao Marcelo do frango ( Uma casa que vende frango - 074999425976 Auxiliadora/ Localizção - 9°50'37.4 S 39°28'28.5 W"},
-    {coords:[-9.843023, -39.483429], name:"Jusciene Dias Rodrigues", whatsapp:"5574999611019", color:'red', local:"Bairro:Alto do Conselheiro - Endereço:Rua Seu Anísio , 40 - Ref.:Próximo ao banco Bradesco / Ao lado da Lurdinha da Avon / 74999945804 - João "},
-    {coords:[-8.897836, -39.772084], name:"Daniela Da Cruz Silva", whatsapp:"5574988193459", color:'red', local:"Bairro:Zona Rural - Endereço:Fazenda Poço do Angico , sn - Ref.:Próximo à casa de Lauzinho "},
-    
+    {coords:[-9.989255, -39.512338], name:"ARTEMIA BARROS DA SILVA", whatsapp:"+55 75 8868-0662", color:'grey', local:"Fazenda Piabas"},
+
 ];

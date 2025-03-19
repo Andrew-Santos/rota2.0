1cdb71c2ad7827691b7a6e1956d9309b84f2e10a
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2025-03-19 16:15:32 -0300

    Update locations.js

--- locations.js
+++ locations.js
@@ -2,20 +2,6 @@
 
 
 export const locations = [
-    {coords:[-11.5462082,-38.9713463], name:"Josimara Oliveira Santos", whatsapp:"5575991555234", color:'grey', local:"Bairro: Zona Rural - Endereço: Povoado Lagoa do Ramo, sn - Ref.:Atrás da Igreja Assembleia de Deus // Em frente a Igreja Católica // (75) 99156-6282 (João) // "},
     {coords:[-11.345513, -38.882775], name:"Marcos Silva Santos", whatsapp:"5575991668600", color:'grey', local:"Bairro: Zona rural - Endereço: Povoado Serra Branca, sn - Ref.:Ao lado do bar do verão, e do lado esquerdo da igreja do sétimo dia - (75) 99925-4977 (Bruna)"},
-    {coords:[-11.314188, -39.048405], name:"Eliene Andrade Da Cruz", whatsapp:"5575999733559", color:'red', local:"Bairro: Rufino - Endereço: Lagoa da onça, sn - Ref.:A entrada fica perto de uma lagoa logo antes de chegar no povoado de lagoa da onça/ Contato 2: (75) 99896-8971 (Titular do agendamento)"},
-    {coords:[-11.260837, -38.968923], name:"Joao Batista Firmo Carneiro", whatsapp:"5575998400575", color:'red', local:"Bairro: ZONA RURAL - Endereço: POVOADO LAGOA DO CURRAL, S/N - Ref.:Ao lado da Casa do miliano // Em frente a Casa de profeta // (75) 98886-7827 - Claudiane "},
-    {coords:[-11.2283507,-39.1748604], name:"Diego Santos Dias", whatsapp:"5575998522067", color:'red', local:"Bairro: TAPUIO - Endereço: RUA DOIS DE JULHO , S/N - Ref.:PRÓX AO POSTO DE GASOLINA EM UM CERCADO DE FLECHA/2° CONTATO:75998827611 LAILZA"},
-    {coords:[-11.227559, -39.176593], name:"Renilda Miranda Pinho", whatsapp:"5575992270654", color:'red', local:"Bairro: Tapuio - Endereço: Rua C, 15 - Ref.:Na Rua do sindicato no fundo do bar do Renato / Andrea  75998793255  / Ao lado do sindicato dos trabalhadores / "},
-    {coords:[-11.177486, -38.944493], name:"Josean De Sousa", whatsapp:"5575991638098", color:'grey', local:"Bairro: Zona rural - Endereço: Povoado barreiro Preto, sn - Ref.:Perto do Bar do Marcinho/Do lado mercado da kele/75998900060  Andresa"},
-    {coords:[-11.140466, -39.169759], name:"Evanilde Reis Da Silva", whatsapp:"5575981237013", color:'grey', local:"Bairro: Pedra Alta - Endereço: rua Ponciano Reis, sn - Ref.:ao lado do posto dá polícia /  em frente a padaria do ze da Raimunda / 75998130513 - Mimia"},
-    {coords:[-11.128786, -39.225365], name:"Fabiana Santos Monteiro", whatsapp:"5575983732513", color:'grey', local:"Bairro: Zona Rural  - Endereço: Provado Roça de Dentro, sn - Ref.:Roça de Dentro, casa do Lando,Rua da palha"},
-    {coords:[-11.126082, -39.246319], name:"Ana Paula Jesus Dos Santos", whatsapp:"5575999107045", color:'red', local:"Bairro: Povoado Lajinha  - Endereço: Povoado Lajinha, sn - Ref.:Padaria do Catarino / Bar do rosa / Do lado da oficina mecânica do Zé da Tina / (75)998500632 - Tainá "},
-    {coords:[-11.109551, -39.014371], name:"Siene Cruz Souza", whatsapp:"5575998638244", color:'grey', local:"Bairro: João Vieira - Endereço: Povoado Lagoa da Picada, 60 - Ref.:Próxima ao Campo do dinho // Próximo a Vila Madalena // (75) 99910-9411 (Da beneficiária) // "},
-    {coords:[-11.109329, -38.993911], name:"Alicia Andrade De Goes", whatsapp:"5575991874232", color:'grey', local:"Bairro: Lagoa da Jurema - Endereço: Povoado Lagoa da Jurema , sn - Ref.:Fica em frente ao campo de futebol do povoado lagoa da Jurema / Fone: (75) 99106-4524, Silvana / Ao lado de um bar do cassio "},
-    {coords:[-11.105250, -38.976616], name:"Laura Goes De Lima", whatsapp:"557592604791", color:'grey', local:"Bairro: Zona rural - Endereço: Povoado jitirana, S/N - Ref.:Próximo lagoa do boi"},
-    {coords:[-11.104980, -38.976666], name:"Andrea Lima Sousa", whatsapp:"5575991328169", color:'grey', local:"Bairro: ZONA RURAL - Endereço: POVOADO JITIRANA, S/N - Ref.:Próximo lagoa do boi"},
-    {coords:[-11.092012, -38.998062], name:"Monica Ferreira Da Silva", whatsapp:"5575991742785", color:'grey', local:"Bairro: ZONA RURAL - Endereço: POVOADO LAGOA DO BOI, 100 - Ref.: VIZINHA A NEIDE/ PRÓXIMO AO POVOADO/ TEM UM PÉ DE ÁRVORE EM FRENTE A CASA."},
 
 ];

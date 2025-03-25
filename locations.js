138de255e61d9775c7bd6eccf7de95cd09cefd5b
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2025-03-25 17:06:31 -0300

    Update locations.js

--- locations.js
+++ locations.js
@@ -4,15 +4,20 @@
 export const locations = [
     {coords:[-12.052014064230775, -40.273927422371266], name:"Ioneide Silva Soares De Almeida", whatsapp:"5574999124998", color:'grey', local:"Bairro: Zona rural - Endereço: Fazenda PO Judia, SN - Ref.:Próx. a Fazenda Ondina / Próx. a Escola Jose de Anchieta - (74) 99809-9633 (sua filha Viviane)"},
     {coords:[-12.095890371015056, -40.23811109999958], name:"Renilto Machado Pereira", whatsapp:"5574999894200", color:'grey', local:"Bairro: Zona rural - Endereço: Fazenda São Bento  , SN - Ref.:Próximo ao distrito de Nova cruz / Próximo a Igreja de São José - (71) 99648-1881 (sua cunhada Jamile)"},
+    {coords:[-12.106824, -40.239295], name:"Bibiana Nepomuceno Ribeiro", whatsapp:"557499416578", color:'red', local:"Bairro:  - Endereço: ,  - Ref.:"},
     {coords:[-12.135517, -40.356005], name:"Luciene Da Silva Araujo Pereira", whatsapp:"5574998054670", color:'grey', local:"Bairro:  CENTRO - Endereço:  RUA DR SEVERINO VIEIRA, 149 - Ref.:PROX AO BAR DO ZÉ  /  PROX A BORRACHARIA COBOGE MOTOS  /   74998005455 - ADMILSOM  , "},
+    {coords:[-12.135940, -40.356562], name:"Jessica Brito Souza", whatsapp:"5574999151537", color:'red', local:"Bairro:  - Endereço: ,  - Ref.:"},
     {coords:[-12.136189, -40,390373], name:"Iranice Dos Santos Evangelista", whatsapp:"5574998063507", color:'red', local:"Bairro: Zona rural - Endereço: sitio vargem alta, sn - Ref.:Próximo a Zé capitão / Visinho a Brolino (74) 99929-8427 filho Jucelio)"},
     {coords:[-12.139135, -40.361063], name:"Ana Lucia Borges De Oliveira", whatsapp:"5574999182505", color:'grey', local:"Bairro: Centro - Endereço: Fazenda Verbena, sn - Ref.:Em frente à igreja// Na rua da delegacia em frente//  61 99880-4141 Ana"},
     {coords:[-12.139595, -40.249104], name:"Claudinea De Almeida Nunes", whatsapp:"5574999319807", color:'grey', local:"Bairro: Nova cruz - Endereço: Rua velha , sn - Ref.:De frente o seminário velho"},
     {coords:[-12.140930, -40.249572], name:"Janete Pedreira Dias", whatsapp:"5574999035408", color:'grey', local:"Bairro: Nova Cruz - Endereço: Avenida 2 de julho 122, sn - Ref.:Ao lado da escola Eurides teles são leão"},
     {coords:[-12.141011471497777, -40.252683672855085], name:"Ozana Queiroz Cardeal", whatsapp:"5574999041159", color:'grey', local:"Bairro: Nova Cruz - Endereço: Rua Zotico Pereira, sn - Ref.:Do lado do laboratório de exames Labclin // Próximo a casa de Terisvaldo Transportes //  (74) 99979-8034 Edi Carlos "},
     {coords:[-12.143945, -40.360735], name:"Elaine Pamponet Vieira", whatsapp:"5574999334508", color:'grey', local:"Bairro: Centro  - Endereço: Loteamento josapha Brandão, 09 - Ref.:Em frente a padaria de Diego "},
+    {coords:[-12.146123, -40.363968], name:"Marinalva Miranda De Lima", whatsapp:"557499528620", color:'red', local:"Bairro:  - Endereço: ,  - Ref.:"},
     {coords:[-12.147032546993707, -40.36499592958625], name:"Juscilandio Silva Dos Santos", whatsapp:"5575999447275", color:'red', local:"Bairro: Centro - Endereço: Rua filito Soares Sampaio , 1995 - Ref.:Na rua da delegacia/  Proximo a raimundo Moreira /  71 99961-0952 dene primo "},
+    {coords:[-12.147389, -40.364953], name:"Cristiano Jesus Da Silva", whatsapp:"557499426844", color:'red', local:"Bairro:  - Endereço: ,  - Ref.:"},
     {coords:[-12.159510, -40.389482], name:"Odivania Silva De Nunes Magalhaes", whatsapp:"5574999965303", color:'red', local:"Bairro: Zona rural - Endereço: Sítio Caldeirão, Sn - Ref.:Ponto do requeijão/próximo a fazenda São José/ 74999651236 Ivanilton (esposo)"},
+    {coords:[-12.171929, -40.204391], name:"Maria De Fatima Souza Dos Santos", whatsapp:"557499567912", color:'red', local:"Bairro:  - Endereço: ,  - Ref.:"},
     {coords:[-12.172391, -40.251357], name:"Antonia Souza Da Conceicao", whatsapp:"5574999067599", color:'grey', local:"Bairro: ZONA RURAL - Endereço: FAZENDA LAGOA DA BARRA, S/N - Ref.:PROX A ESTRADA DO TANQUINHO / PROX A ALTO FARINHA BOA / PROX  A NOVA CRUZ "},
 
 ];

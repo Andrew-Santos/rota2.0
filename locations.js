97af8639884aa19e3115d24845294420c7d897f0
Andrew Santos <89900236+Andrew-Santos@users.noreply.github.com>
2025-03-25 16:28:02 -0300

    Update locations.js

--- locations.js
+++ locations.js
@@ -2,6 +2,10 @@
 
 
 export const locations = [
-    {coords:[-11.345513, -38.882775], name:"Marcos Silva Santos", whatsapp:"5575991668600", color:'grey', local:"Bairro: Zona rural - Endereço: Povoado Serra Branca, sn - Ref.:Ao lado do bar do verão, e do lado esquerdo da igreja do sétimo dia - (75) 99925-4977 (Bruna)"},
+    {coords:[-12.139135, -40.361063], name:"Ana Lucia Borges De Oliveira", whatsapp:"5574999182505", color:'grey', local:"Bairro: Centro - Endereço: Fazenda Verbena, sn - Ref.:Em frente à igreja// Na rua da delegacia em frente//  61 99880-4141 Ana"},
+    {coords:[-12.095890371015056, -40.23811109999958], name:"Renilto Machado Pereira", whatsapp:"5574999894200", color:'grey', local:"Bairro: Zona rural - Endereço: Fazenda São Bento  , SN - Ref.:Próximo ao distrito de Nova cruz / Próximo a Igreja de São José - (71) 99648-1881 (sua cunhada Jamile)"},
+    {coords:[-12.052014064230775, -40.273927422371266], name:"Ioneide Silva Soares De Almeida", whatsapp:"5574999124998", color:'grey', local:"Bairro: Zona rural - Endereço: Fazenda PO Judia, SN - Ref.:Próx. a Fazenda Ondina / Próx. a Escola Jose de Anchieta - (74) 99809-9633 (sua filha Viviane)"},
+    {coords:[-12.141011471497777, -40.252683672855085], name:"Ozana Queiroz Cardeal", whatsapp:"5574999041159", color:'grey', local:"Bairro: Nova Cruz - Endereço: Rua Zotico Pereira, sn - Ref.:Do lado do laboratório de exames Labclin // Próximo a casa de Terisvaldo Transportes //  (74) 99979-8034 Edi Carlos "},
+    {coords:[-12.147032546993707, -40.36499592958625], name:"Juscilandio Silva Dos Santos", whatsapp:"5575999447275", color:'red', local:"Bairro: Centro - Endereço: Rua filito Soares Sampaio , 1995 - Ref.:Na rua da delegacia/  Proximo a raimundo Moreira /  71 99961-0952 dene primo "},
 
 ];
